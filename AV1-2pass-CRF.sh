1b8418df3f2d0930e3e59959c5b87a2ddb6b6efd
Nova-Aurora <62871042+Nova-Aurora@users.noreply.github.com>
2020-06-02 20:28:04 -0500

    added a fix for experimental status of libaom

--- AV1-2pass-CRF.sh
+++ AV1-2pass-CRF.sh
@@ -7,5 +7,5 @@ read -p "CRF?
 " crf
 name=`echo "$in-AV1" | cut -d'.' -f1`
   echo "$name"
-  ffmpeg -hwaccel auto -i "$in" -an -c:v libaom-av1 -b:v 0 -crf $crf -pass 1 -f webm /dev/null && \
-  ffmpeg -hwaccel auto -i "$in" -c:a libopus -c:v libaom-av1 -b:v 0 -pass 2 -crf $crf "./${name}.webm"
+  ffmpeg -strict -2 -hwaccel auto -i "$in" -an -c:v libaom-av1 -b:v 0 -crf $crf -pass 1 -f webm /dev/null && \
+  ffmpeg -strict -2 -hwaccel auto -i "$in" -c:a libopus -c:v libaom-av1 -b:v 0 -pass 2 -crf $crf "./${name}.webm"
